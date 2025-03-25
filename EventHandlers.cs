23c594ce72762ce03df679bc896e9b622830754e
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2025-03-25 22:48:36 +0300

    .

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,6 +1,5 @@
 ﻿using Exiled.API.Features;
 using Exiled.Events.EventArgs.Warhead;
-using PlayerStatsSystem;
 
 namespace DeadmanSwitchRemover
 {
@@ -10,13 +9,13 @@ namespace DeadmanSwitchRemover
         public EventHandlers(Plugin plugin) => this.plugin = plugin;
         public void DeadmenSwitchInit (DeadmanSwitchInitiatingEventArgs ev)
         {
-            if (Round.ElapsedTime.TotalMinutes < plugin.Config.DelayMinutes)
-            {
-                ev.IsAllowed = false;
-                DeadmanSwitch._dmsTime += 10f;// I know i can just reject it but this time the event is triggered 35 times per second and the logs accumulate too much
-                Log.Debug("DMS has been delayed for 10 seconds because min required time is " + plugin.Config.DelayMinutes);
-            }
-               
+            if (Round.ElapsedTime.TotalMinutes >= plugin.Config.DelayMinutes)
+                return;
+
+            ev.IsAllowed = false;
+            DeadmanSwitch._dmsTime += 5f;// I know i can just reject it but this time the event is triggered 35 times per second and the logs accumulate too much
+            Log.Debug("DMS has been delayed for 5 seconds because min required time is " + plugin.Config.DelayMinutes);
+
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -7,32 +7,32 @@ namespace DeadmanSwitchRemover
     {
         public static EventHandlers eventHandlers;
         public static Plugin Instance { get; private set; }
-
         public override string Author => "ZurnaSever";
-
         public override string Name => "DmsRemover";
-
         public override string Prefix => "DmsRemover";
-
         public override Version RequiredExiledVersion { get; } = new Version(9, 5, 1);
-
         public override Version Version { get; } = new Version(1, 5, 0);
+
         public override void OnEnabled()
         {
             Instance = this;
-            eventHandlers = new EventHandlers(this);
-            if (!Config.PermenantlyRemove) Exiled.Events.Handlers.Warhead.DeadmanSwitchInitiating += eventHandlers.DeadmenSwitchInit;
+            if (!Config.PermenantlyRemove) 
+            {
+                eventHandlers = new EventHandlers(this);
+                Exiled.Events.Handlers.Warhead.DeadmanSwitchInitiating += eventHandlers.DeadmenSwitchInit;
+            }
             else 
             { 
                 StaticUnityMethods.OnUpdate -= (Action)DeadmanSwitch.OnUpdate; 
                 Log.Debug("DMS has been permanently disabled");
             }
-            
             base.OnEnabled();
         }
         public override void OnDisabled()
         {
-            if (!Config.PermenantlyRemove) Exiled.Events.Handlers.Warhead.DeadmanSwitchInitiating -= eventHandlers.DeadmenSwitchInit;
+            if (!Config.PermenantlyRemove) 
+                Exiled.Events.Handlers.Warhead.DeadmanSwitchInitiating -= eventHandlers.DeadmenSwitchInit;
+
             eventHandlers = null;
             Instance = null;
             base.OnDisabled();
