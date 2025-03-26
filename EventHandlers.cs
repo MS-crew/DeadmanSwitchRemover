bc4dfa79096bdef38c039f9e18d73a0d6cadb195
MS-crew <100300664+MS-crew@users.noreply.github.com>
2025-03-26 04:53:12 +0300

    Update EventHandlers.cs
    Delete space

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -15,7 +15,6 @@ namespace DeadmanSwitchRemover
             ev.IsAllowed = false;
             DeadmanSwitch._dmsTime += 5f;// I know i can just reject it but this time the event is triggered 35 times per second and the logs accumulate too much
             Log.Debug("DMS has been delayed for 5 seconds because min required time is " + plugin.Config.DelayMinutes);
-
         }
     }
 }
